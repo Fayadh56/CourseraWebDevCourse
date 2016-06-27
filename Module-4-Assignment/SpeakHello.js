f1e7a20934b59fd04691d89b2dbef1e5b1b7af28
Fayadh A <fayadh56@gmail.com>
2016-06-27 22:29:15 +0400

    Update SpeakHello.js

--- Module-4-Assignment/SpeakHello.js
+++ Module-4-Assignment/SpeakHello.js
@@ -1,13 +1,14 @@
+
 // Fayadh Ahmed - Coursera Assignment 4 - 27-June-2016
 
 (function (window) {
 	// create obj helloSpeaker, with the function speak(name) inside of it
 	var helloSpeaker = {
-		speak(name) { console.log(speakWord + " " + name); }
-		
+		speak(name) {
+	  		console.log(speakWord + " " + name);
+		}
 	}
 	// expose it to global, using window
 	window.helloSpeaker = helloSpeaker;
 	var speakWord = "Hello";
 })(window);
-
