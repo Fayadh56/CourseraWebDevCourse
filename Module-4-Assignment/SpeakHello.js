9eb4455f90602b957aa8a412b04ae1a3b09ccaa7
Fayadh A <fayadh56@gmail.com>
2016-06-27 22:26:03 +0400

    key value pair fix

--- Module-4-Assignment/SpeakHello.js
+++ Module-4-Assignment/SpeakHello.js
@@ -3,9 +3,8 @@
 (function (window) {
 	// create obj helloSpeaker, with the function speak(name) inside of it
 	var helloSpeaker = {
-		speak(name) {
-	  		console.log(speakWord + " " + name);
-		}
+		speak(name) : console.log(speakWord + " " + name);
+		
 	}
 	// expose it to global, using window
 	window.helloSpeaker = helloSpeaker;
